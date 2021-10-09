d65a2bd5953cb3c143e100990a79a4c8df9a2dad
Konrad Gnat <konradmgnat@gmail.com>
2021-10-09 07:50:48 +0200

    add job marketplace contract structs, and functions

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -4,13 +4,86 @@ import { Layout, Row, Col } from "antd";
 import LandingLayout from "../../components/layout/LandingLayout";
 import CommunityData from "../../utilities/communities.json";
 import CommunityCard from "../../components/shared/CommunityCard";
+import { useEffect } from "react-router/node_modules/@types/react";
+import Web3Modal from 'web3modal';
 
 const { Content } = Layout;
 
+declare global {
+  interface Window {
+    ethereum: any
+  }
+}
 interface Props {}
 //const CommunityData = CommunityData.slice(0, 3);
 
 const Home = (props: Props) => {
+
+  let carol: any;
+
+  useEffect(() => {
+    initialize();
+  }, [])
+
+  const initialize = async () => {
+    const SuperfluidSDK = require("@superfluid-finance/js-sdk");
+    const { Web3Provider } = require("@ethersproject/providers");
+
+
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    // const provider = new ethers.providers.Web3Provider(connection);
+
+
+    console.log('window.ethereum: ', window.ethereum);
+    console.log('pre SuperfluidSDK');
+    const sf = new SuperfluidSDK.Framework({
+        ethers: new Web3Provider(connection)
+    });
+    console.log('pre initialized');
+    await sf.initialize()
+    console.log('initialized');
+
+    const walletAddress = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+      params: [
+        {
+          eth_accounts: {}
+        }
+      ]
+    });
+
+    console.log('addr: ', walletAddress);
+        
+    carol = sf.user({
+        address: walletAddress[0],
+        token: '0xF2d68898557cCb2Cf4C10c3Ef2B034b2a69DAD00'
+    });
+    const details = await carol.details();
+    console.log(details);
+
+  }
+  const makeFlow = async () => {
+    // 0x2880f6f8a913841ea336e58459821c8f25f97470
+      await carol.flow({
+        recipient: '0x2880f6f8a913841ea336e58459821c8f25f97470',
+        flowRate: '385802469135802'
+    });
+    
+    const details = await carol.details();
+    console.log(details);
+  }
+  const stopFlow = async () => {
+    // 0x2880f6f8a913841ea336e58459821c8f25f97470
+    await carol.flow({
+      recipient: '0x2880f6f8a913841ea336e58459821c8f25f97470',
+      flowRate: '0'
+  });
+  
+  const details = await carol.details();
+  console.log('stop: ', details);
+  }
+
   return (
     <LandingLayout>
       <Content>
@@ -20,7 +93,7 @@ const Home = (props: Props) => {
             <div className="see-all">
               <Link to="/communities">See All</Link>
             </div>
-            <h3 className="heading">"Communities"</h3>
+            <h3 className="heading">"Jobs"</h3>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify="center">
               {CommunityData.slice(0, 3).map((community, index) => (
                 <Col span={6}>
--- contracts/JobMarketplace.sol
+++ contracts/JobMarketplace.sol
@@ -4,3 +4,115 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
+import "hardhat/console.sol";
+
+contract VaYemMarketplace is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _accountIds;
+    Account[] public accounts;
+
+    address payable owner;
+    uint256 accountCreationPrice = 0.0025 ether;
+
+    mapping(uint256 => Account) private idToAccount;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct Job {
+        uint jobId;
+        uint accountId;
+        string description;
+        address acc_owner;
+        uint pricePerWeek;
+        uint maxUnits;
+        bool active;
+    }
+
+    struct Account {
+        uint accountId;
+        string description;
+        string imageUrl;
+        address payable acc_owner;
+        Job[] jobs;
+        uint256 jobIds;
+    }
+
+    event AccountCreated (
+        uint indexed accountId,
+        string description,
+        string imageUrl,
+        address acc_owner,
+        Job[] jobs,
+        uint256 jobIds
+    );
+
+    event JobCreated (
+        uint indexed jobId,
+        uint indexed accountId,
+        string description,
+        address acc_owner,
+        uint pricePerWeek,
+        uint maxUnits,
+        bool active
+    );
+    
+    function createAccount() public payable nonReentrant {
+        require(msg.value == accountCreationPrice, "Price must equal to account creation price");
+        _accountIds.increment();
+        uint256 accountId = _accountIds.current();
+
+        idToAccount[accountId] = Account(
+            accountId,
+            description,
+            imageUrl,
+            payable(msg.sender),
+            [],
+            0
+        );
+
+        emit AccountCreated(
+            accountId,
+            description,
+            imageUrl,
+            payable(msg.sender),
+            []
+        );
+    }
+
+    function createJob(
+        uint256 accountId,
+        string description,
+        uint256 pricePerWeek,
+        uint256 maxUnits
+    ) public payable nonReentrant {
+        address acc_owner = idToAccount[accountId].acc_owner;
+        require(msg.sender == acc_owner, "Only account owner can create new jobs");
+
+        uint256 newJobId = idToAccount[accountId].jobIds + 1;
+        Job newJob = Job(
+            newJobId,
+            accountId,
+            description,
+            acc_owner,
+            pricePerWeek,
+            maxUnits,
+            true
+        );
+
+        emit JobCreated(
+            newJobId,
+            accountId,
+            description,
+            acc_owner,
+            pricePerWeek,
+            maxUnits,
+            true
+        );
+    }
+
+    fetchAccounts() public view returns (Accounts[] memory) {
+        
+    }
+}
